927705d1f52606590183b7ce4e50eb73039466d2
Vincent McAulay <bwownbwead@hotmail.com>
2020-08-15 21:26:17 +0100

    Added basic nav and site wrap

---
+++ components/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <nuxt-link to="/">
+                    Home
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/about">
+                    About
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/about">
+                    About
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/contact">
+                    Contact
+                </nuxt-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'navigation'
+    }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,29 @@
 <template>
-	<div>
+	<div class="site-wrap">
+        <div class="mb-6">
+            <navigation />
+        </div>
 		<Nuxt />
 	</div>
 </template>
 
+<script>
+    import Navigation from '~/components/Navigation';
+
+    export default {
+        components: {
+            Navigation
+        }
+    }
+</script>
+
 <style lang="scss">
     html {
         font-family: 'Source Sans Pro', sans-serif;
+    }
+
+    .site-wrap {
+        max-width: 1070px;
+        margin: 0 auto;
     }
 </style>
