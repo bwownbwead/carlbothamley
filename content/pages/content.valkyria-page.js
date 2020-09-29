e5a56d9e83296ef27cdead4e346f548ebe5b1078
Vincent McAulay <bwownbwead@hotmail.com>
2020-09-29 12:20:57 +0100

    added valkyria page

---
+++ content/pages/content.valkyria-page.js
@@ -0,0 +1,11 @@
+
+export default {
+    heading: "Liseberg: Valkyria",
+    subHeading: "Concept Art_Model_Texture_Animation",
+    intro: "Promotional material from concept art to coaster animation.",
+    body: [
+        "Valkyria is Europe’s longest Dive Coaster, a roller coaster with a vertical drop of nearly 50 metres. Just as you’re about to go over the edge, the ride pauses and you plunge vertically into an underground tunnel at a speed of 105 kph. The Coaster is situated within the Myths and Legend area of Liseberg Theme Park in Sweden.",
+        "Along with Quarry Fold Studio I created concept material based around the theming of Norse mythology to promote Valkyria. I also created multiple ride animations, one in situ within Liseberg and one in a fantasy environment that fitted within the ride theme. The work created by myself was used in Print, TV and web."
+    ]
+
+}
--- pages/valkyria/index.vue
+++ pages/valkyria/index.vue
@@ -1,11 +1,55 @@
 <template>
-	<div>
-		Valkyria
-	</div>
+    <div>
+        <page-headings
+            :heading="content.heading"
+            :subHeading="content.subHeading"
+        />
+
+        <page-intro
+            v-if="content.intro"
+            :intro="content.intro"
+        />
+
+        <!-- Carousel -->
+
+        <body-copy
+            :body="content.body"
+        />
+
+        <div class="my-8">
+            <responsive-you-tube
+                videoSrc="https://www.youtube.com/embed/E3iMSvMYcvA"
+            />
+        </div>
+
+        <p><a href="https://www.liseberg.com/valkyria/" target="_blank">https://www.liseberg.com/valkyria/</a></p>
+        <p><a href="https://www.youtube.com/user/Cyaarl/videos" target="_blank">https://www.youtube.com/user/Cyaarl/videos</a></p>
+
+        <back-to-work-link />
+    </div>
 </template>
 
 <script>
-	export default {
 
+    import PageHeadings from '~/components/PageHeadings';
+    import PageIntro from '~/components/PageIntro';
+    import BodyCopy from '~/components/BodyCopy';
+    import BackToWorkLink from '~/components/BackToWorkLink';
+    import ResponsiveYouTube from '~/components/ResponsiveYouTube';
+    import content from '~/content/pages/content.valkyria-page';
+
+    export default {
+        components: {
+            PageHeadings,
+            PageIntro,
+            BodyCopy,
+            ResponsiveYouTube
+        },
+
+        data() {
+            return {
+                content
+            }
+        }
     }
 </script>
