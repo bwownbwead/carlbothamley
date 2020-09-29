2f0b3f1283167a6c8fc0fa6a27afeb1375cafd21
Vincent McAulay <bwownbwead@hotmail.com>
2020-09-29 11:04:38 +0100

    added contact page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,11 +1,27 @@
 <template>
 	<div>
-		Contact
+		<page-headings
+            heading="Contact"
+            subHeading="Ring-a-ding-ding"
+        />
+
+        <p class="mb-4">If you want to request information about my services or just want to say hello then shoot me an email.</p>
+
+        <p class="mb-6">
+            <a class="text-xl font-semibold border-b-2 hover:border-b-0" href="mailto:hello@carlbothamley.com">Hello@carlbothamley.com</a>
+        </p>
+
+        <p>I look forward to hearing from you.</p>
 	</div>
 </template>
 
 <script>
-	export default {
 
+    import PageHeadings from '~/components/PageHeadings';
+
+	export default {
+        components: {
+            PageHeadings            
+        }
     }
 </script>
