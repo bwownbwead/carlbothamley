04757b3cf51c9249c145740be3dd4f2ea5bdd78a
Vincent McAulay <bwownbwead@hotmail.com>
2020-10-02 08:39:56 +0100

    added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
 			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
 		],
 		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.gif' },
 			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap' }
 		]
 	},
